ea900485a179a5a358879cf70c1d839484e777b1
Heli Devine <heli.devine@gmail.com>
2021-12-29 10:14:01 +0000

    changed code to deploy with Heroku

--- src/services/db.js
+++ src/services/db.js
@@ -1,19 +1,39 @@
 /// before deploying with heroku///
 
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 
-const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
+// const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
 
-module.exports = async () => {
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_NAME
-  });
+// module.exports = async () => {
+//   const connection = await mysql.createConnection({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     port: DB_PORT,
+//     database: DB_NAME
+//   });
 
-  return connection;
-};
+//   return connection;
+// };
 
-/// before deploying with heroku///
+/// after deploying with heroku///
+
+const db = CLEARDB_DATABASE_URL
+  ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+  : await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+      database: DB_NAME
+    });
+
+// create the database if it doesn't already exist
+!CLEARDB_DATABASE_URL && (await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`));
+!CLEARDB_DATABASE_URL && (await db.query(`USE ${DB_NAME}`));
+await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+              id INT PRIMARY KEY auto_increment,
+              name VARCHAR(25),
+              genre VARCHAR(25)
+             )`);
+db.close();
