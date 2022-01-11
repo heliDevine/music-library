61f7603b08e2b05a7411679085c21687641e01fa
Heli Devine <heli.devine@gmail.com>
2022-01-11 19:49:40 +0000

    added swagger files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
+
 const artistRouter = require('./routes/artist');
-const artistController = require('./controllers/artist');
+// const artistController = require('./controllers/artist');
 
 const app = express();
 app.use(express.json());
 
 app.use('/artist', artistRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // app.get('/', (req, res) => {
 //   res.status(200).send('Hello World!')
